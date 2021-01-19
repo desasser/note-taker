85dc42ce625de93567a055616b3bc59de7aa0453
Daniel Sasser <desasser79@gmail.com>
2021-01-19 15:23:49 -0800

    fixed refresh bugs

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,15 +2,21 @@ let noteData = require('../db/db.json');
 const fs = require('fs');
 const { stringify } = require('querystring');
 const e = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 module.exports = function (app) {
   app.get("/api/notes", function (req, res) {
-    res.json(noteData);
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) throw err;
+      res.json(JSON.parse(data));
+    })
   });
 
   app.post("/api/notes", function (req, res) {
     // recieve new note to save
     const newNote = req.body;
+    newNote.id = uuidv4();
+
     // add it to the db.json file
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
       if (err) throw err;
@@ -24,12 +30,11 @@ module.exports = function (app) {
       // write the new string into the file, overwriting the previous version
       fs.writeFile('./db/db.json', dataWrite, (err) => {
         if (err) throw err;
-        console.log('saved succesfully!');
+
+        // return the new note to the client
+        res.send(newNote);
       });
     });
-
-    // return the new note to the client
-    return newNote;
   });
 
   app.delete("/api/notes/:id", function (req, res) {
@@ -38,29 +43,21 @@ module.exports = function (app) {
     // read notes
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
       if (err) throw err;
-
       const newArrDel = JSON.parse(data);
-      // console.log('before', newArrDel);
-      let i = 0;
-      // console.log('id', parseInt(id));
+
       // for each note, check to see if it matches the ID and delete it
-      newArrDel.forEach((element) => {
-        i++;
-        element.id = i;
-        if (element.id === parseInt(id)) {
+      newArrDel.forEach((element, i) => {
+
+        if (id === element.id) {
           // delete it
-          // console.log('before', element);
-          // this is not deleting the element from the array
-          newArrDel.splice(i-1,1);
-          // console.log('after', element);
+          const test = newArrDel.splice(i, 1);
         };
       });
-      // console.log('before', newArrDel);
 
       const dataWriteDel = JSON.stringify(newArrDel);
       fs.writeFile('./db/db.json', dataWriteDel, (err) => {
         if (err) throw err;
-        console.log('saved succesfully!');
+        res.json(true);
       });
     });
   });
