0681ce9936c9558d2e3f82a613e340c9c387710f
Daniel Sasser <desasser79@gmail.com>
2021-01-18 17:08:02 -0800

    newArr is updating during .post

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,47 @@
 let noteData = require('../db/db.json');
 const fs = require('fs');
+const { stringify } = require('querystring');
 
 module.exports = function(app) {
   app.get("/api/notes", function(req, res) {
     res.json(noteData);
   });
 
+  //TODO: syntax for read/write file, returning newNote?
   app.post("/api/notes", function(req, res) {
-    
+    // recieve new note to save
+    const newNote = req.body;
+    // add it to the db.json file
+    const newArr = fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) throw err;
+      
+      console.log('this is data', JSON.parse(data));
+      console.log('im a new note', newNote);
+      const newArr = JSON.parse(data);
+      newArr.push(newNote);
+      console.log(newArr);
+    });
+
+    fs.writeFile('./db/db.json', data, (err) => {
+      if (err) throw err;
+      console.log('saved succesfully!');
+    });
+    // return the new note to the client
+    return newNote;
   });
 
   app.delete("/api/notes/:id", function(req, res) {
     let id = req.params.id;
+    // read notes
+    const newArr = fs.readFile('./db/db.json', (err, data) => {
+      if (err) throw err;
+    });
+    // for each note, check to see if it matches the ID and delete it
+    newArr.forEach((element) => {
+      if (id = element.id) {
+        //delete it
+        delete element;
+      };
+    });
   });
-}
+};
